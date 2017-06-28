308823c2795d0d798148b0782fd724c9490863a2
hscottharrison <h.scott.harrison@tcu.edu>
2017-06-27 22:34:21 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 3001;
+const cors = require('cors');
+const app = express();
+const massive = require('massive');
+const masterRoutes = require ('./server/masterRoutes');
+const session = require('express-session');
+const config = require('./server/config');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(session(config.session));
+app.use('/', express.static(__dirname + '/public'));
+massive(config.postgres).then(dbInstance => {
+  app.set('db', dbInstance)
+});
+// masterRoutes(app);
+
+
+app.listen(port, () => {
+  console.log('Whatchu want?');
+});
